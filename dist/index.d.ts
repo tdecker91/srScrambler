9660ca811fb177222b9de62e5521317f92236062
Tyson Decker <tyson.decker@divvypay.com>
2022-03-09 00:01:55 -0700

    add dist

---
+++ dist/index.d.ts
@@ -0,0 +1,6 @@
+export declare const cube: (size?: number, numTurns?: number) => String;
+export declare const megaminx: () => String;
+export declare const pyraminx: (numTurns?: number) => String;
+export declare const skewb: (numTurns?: number) => String;
+export declare const square1: (numTurns?: number) => any;
+//# sourceMappingURL=index.d.ts.map
---
+++ dist/util.d.ts
@@ -0,0 +1,3 @@
+export declare function randomElement<T>(array: T[]): T;
+export declare function shuffle<T>(array: T[]): T[];
+//# sourceMappingURL=util.d.ts.map
